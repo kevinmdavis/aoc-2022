c2111c966f14622cfba42ea82cd484ce316a35a4
Kevin Davis <kevin@kdavis.dev>
2022-12-03 08:23:18 -0600

    Solution for day 3.

---
+++ src/Day03.kt
@@ -0,0 +1,42 @@
+import java.io.File
+
+typealias ItemSet = Set<Char>
+
+fun String.compartments(): List<ItemSet> {
+    val mid = length / 2
+    val first = substring(0, mid).toCharArray().toSet()
+    val second = substring(mid).toCharArray().toSet()
+    return listOf(first, second)
+}
+
+fun List<String>.groups(): List<List<ItemSet>> {
+    return chunked(3) { group ->
+        group.map { it.toCharArray().toSet() }
+    }
+}
+
+fun List<ItemSet>.overlap(): Char = reduce { a, b ->
+    a.intersect(b)
+}.single()
+
+fun Char.priority(): Int {
+    if (isLowerCase()) {
+        return code - 'a'.code + 1
+    }
+    if (isUpperCase()) {
+        return code - 'A'.code + 1 + 26
+    }
+    throw IllegalArgumentException()
+}
+
+fun main() {
+    val lines = File("input/day03.txt").readLines()
+    val result1 = lines.sumOf {
+        it.compartments().overlap().priority()
+    }
+    val result2 = lines.groups().sumOf {
+        it.overlap().priority()
+    }
+    println("Part 1: $result1")
+    println("Part 2: $result2")
+}
