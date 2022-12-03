14227f792265de3c32950f5aed3a1a44fddac2b1
Kevin Davis <kevin@kdavis.dev>
2022-12-02 23:05:30 -0600

    Solution for day 1.

---
+++ src/Day01.kt
@@ -0,0 +1,32 @@
+import java.io.File
+
+fun parseCounts(lines: List<String>): List<List<Int>> {
+    val result: MutableList<List<Int>> = mutableListOf()
+    var current: MutableList<Int> = mutableListOf()
+    for (line in lines) {
+        if (line.isBlank()) {
+            result.add(current)
+            current = mutableListOf()
+        } else {
+            current.add(line.toInt())
+        }
+    }
+    result.add(current)
+    return result
+}
+
+fun part1(counts: List<List<Int>>): Int {
+    return counts.maxOf { it.sum() }
+}
+
+fun part2(counts: List<List<Int>>): Int {
+    return counts.map { it.sum() }.sorted().takeLast(3).sum()
+}
+
+fun main() {
+    val counts = parseCounts(File("input/day01.txt").readLines())
+    val result1 = part1(counts)
+    val result2 = part2(counts)
+    println("Part 1: $result1")
+    println("Part 2: $result2")
+}
